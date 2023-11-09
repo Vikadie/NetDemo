0f1ad57cdc50bbf22f799abea42a799c8036007e
Vikadie <68245263+Vikadie@users.noreply.github.com>
2023-11-09 10:57:14 +0200

    .updating the stripe API endpoint version

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -26,12 +26,12 @@ namespace API.Controllers
             _config = config;
             _paymentsService = paymentsService;
             _context = context;
-            
+
         }
 
         [Authorize]
         [HttpPost]
-        public async Task<ActionResult<BasketDto>> CreateOrUpdatePaymentIntent() 
+        public async Task<ActionResult<BasketDto>> CreateOrUpdatePaymentIntent()
         {
             var basket = await _context.Baskets
                 .RetirieveBasketWithItems(User.Identity.Name)
@@ -41,7 +41,7 @@ namespace API.Controllers
 
             var intent = await _paymentsService.CreateOrUpdatePaymentIntent(basket);
 
-            if (intent == null) return BadRequest(new ProblemDetails{Title = "Problem Creating payment intent"});
+            if (intent == null) return BadRequest(new ProblemDetails { Title = "Problem Creating payment intent" });
 
             // as the basket may need to be updated with our new PaymentIntentId 
             // so we set it to the basket along with the ClientSecret
@@ -52,7 +52,7 @@ namespace API.Controllers
             _context.Update(basket);
 
             var result = await _context.SaveChangesAsync() > 0;
-            if (!result) return BadRequest(new ProblemDetails{Title = "Problem updating basket with intent"});
+            if (!result) return BadRequest(new ProblemDetails { Title = "Problem updating basket with intent" });
 
             return basket.MapBasketToDto(); // this will return 201 ok along with the basket
         }
@@ -62,24 +62,31 @@ namespace API.Controllers
         {
             // read the request that Stripe is sending us after the payment has been successfully recieved
             var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-            // to access the StripeEvent we need to create it
-            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], 
-                _config["StripeSettings:WhSecret"]);
+            try
+            {
+                // to access the StripeEvent we need to create it
+                var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"],
+                    _config["StripeSettings:WhSecret"], throwOnApiVersionMismatch: false);
 
-            // access the charge, because we listen to the Charge event (cast to Charge object)
-            var charge = (Charge)stripeEvent.Data.Object;
+                // access the charge, because we listen to the Charge event (cast to Charge object)
+                var charge = (Charge)stripeEvent.Data.Object;
 
-            // from charge we want to get access to PaymentIntentId and get hold of the order from our database that matches this IntentId
-            var order = await _context.Orders.FirstOrDefaultAsync(x => x.PaymentIntentId == charge.PaymentIntentId);
+                // from charge we want to get access to PaymentIntentId and get hold of the order from our database that matches this IntentId
+                var order = await _context.Orders.FirstOrDefaultAsync(x => x.PaymentIntentId == charge.PaymentIntentId);
 
-            // change the status of the charge and if succeeded change the status of our order
-            if (charge.Status == "succeeded") order.OrderStatus = OrderStatus.PaymentReceived;
+                // change the status of the charge and if succeeded change the status of our order
+                if (charge.Status == "succeeded") order.OrderStatus = OrderStatus.PaymentReceived;
 
-            // save changes
-            await _context.SaveChangesAsync();
+                // save changes
+                await _context.SaveChangesAsync();
 
-            // we need to return something to let the Stripe that we have received its request to stop sending it
-            return new EmptyResult();
+                // we need to return something to let the Stripe that we have received its request to stop sending it
+                return new EmptyResult();
+            }
+            catch (StripeException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
